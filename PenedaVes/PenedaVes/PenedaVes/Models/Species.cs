815c4ef8ffd52aa1e957b596251e5bd92d655d8e
jpcorreia99 <jpcorreia99@gmail.com>
2021-06-06 02:53:07 +0100

    change typing to portuguese

--- PenedaVes/PenedaVes/PenedaVes/Models/Species.cs
+++ PenedaVes/PenedaVes/PenedaVes/Models/Species.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace PenedaVes.Models
@@ -7,15 +8,25 @@ namespace PenedaVes.Models
     public class Species
     {
         public int Id { get; set; }
+        
         [Required]
         [StringLength(100)]
+        [DisplayName("Nome Comum")]
         public string CommonName { get; set; }
+        
         [Required]
         [StringLength(100)]
+        [DisplayName("Nome científico")]
         public string ScientificName { get; set; }
+        
+        [DisplayName("Descrição")]
         public string Description { get; set; }
+        
         [Required]
+        [DisplayName("Predatória?")]
         public bool IsPredatory { get; set; }
+        
+        [DisplayName("Link Imagem")]
         [Required]
         [StringLength(100)]
         public string Image { get; set; }
