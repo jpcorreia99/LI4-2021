b88039731b349c9d8d1b491bd316ba6e2b4dbf90
jpcorreia99 <jpcorreia99@gmail.com>
2021-06-11 23:01:51 +0100

    add sample charts

---
+++ PenedaVes/PenedaVes/PenedaVes/ViewModels/BarChart.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+
+namespace PenedaVes.ViewModels
+{
+    public class BarChart
+    {
+        public List<string> Labels { get; set; }
+        public List<BarChartChild> Datasets { get; set; }
+        public BarChart()
+        {
+            Labels = new List<string>();
+            Datasets = new List<BarChartChild>();
+        }
+    }
+
+    public class BarChartChild
+    {
+        public string Label { get; set; }
+        public string BackgroundColor { get; set; }
+        public string BorderColor { get; set; }
+        public int BorderWidth { get; set; }
+        public string HoverBackgroundColor{ get; set; }
+        public string HoverBorderColor{ get; set; }
+        public List<int> Data{ get; set; }
+    }
+    
+    
+}
--- PenedaVes/PenedaVes/PenedaVes/ViewModels/SpeciesDetailsViewModel.cs
+++ PenedaVes/PenedaVes/PenedaVes/ViewModels/SpeciesDetailsViewModel.cs
@@ -18,5 +18,33 @@ namespace PenedaVes.ViewModels
         [Display(Name = "Data de fim")]
         [DataType(DataType.Date)]
         public DateTime EndingDate { get; set; } = DateTime.Now.Date.AddHours(23).AddMinutes(59).AddSeconds(59);
+        
+        public PieChart PieChart { get; set; }
+        public BarChart BarChart { get; set; }
+        
+        
+    }
+    
+    public class PieChart
+    {
+        public List<string> Labels { get; set; }
+        public List<ChildPieChart> Datasets { get; set;}
+        public PieChart()
+        {
+            Labels = new List<string>();
+            Datasets = new List<ChildPieChart>();
+        }
+    }
+
+    public class ChildPieChart
+    {
+        public List<string> BackgroundColor { get; set; } 
+        public List<int> Data { get; set; }
+
+        public ChildPieChart()
+        {
+            BackgroundColor = new List<string>();
+            Data = new List<int>();
+        }
     }
 }
